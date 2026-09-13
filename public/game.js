ff809c69d27db3098d8f56d90ee10581a2782f5d
Ian <ian.li@student.education.wa.edu.au>
2026-09-13 14:19:35 +0800

    gonna implement scrolling later

--- public/game.js
+++ public/game.js
@@ -219,13 +219,6 @@ function update() {
     let scroll = false;
     for (let id in players) {
         if (players[id].y <= 200) {
-            players[id].y = 201;
-            for (let id2 in players) {
-                players[id2].y += players[id].jump;
-            }
-            for (let loop = 0; loop < levelPlatforms.length; loop++) {
-                levelPlatforms[loop].y += players[id].jump;
-            }
             scroll = true;
         }
         if (scroll === true) break;
