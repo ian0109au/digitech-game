ad1a0495ade3baa9afa12a3e236fc1e44eca05a0
Ian <ian.li@student.education.wa.edu.au>
2026-09-13 23:09:54 +0800

    fix

--- public/game.js
+++ public/game.js
@@ -187,10 +187,11 @@ function cameraU() {
     let limit = 0; 
     const leadPlayer = topPlayer(players);
     if (leadPlayer != null) {
-        if (leadPlayer.y < 0 + camera.padding) {
+        const screenY = leadPlayer.y - camera.y;
+        if (screenY < 0 + camera.padding) {
             limit = leadPlayer.y - camera.padding;
         }
-        else if (leadPlayer.y > camera.height - camera.padding) {  
+        else if (screenY > camera.height - camera.padding) {  
             limit = leadPlayer.y - camera.height + camera.padding;
         }
         else {
