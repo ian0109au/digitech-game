404a6e992bb12a57fbfabdf9186866bc387cb964
Ian <ian.li@student.education.wa.edu.au>
2026-09-14 21:23:57 +0800

    work pls

--- public/game.js
+++ public/game.js
@@ -293,7 +293,7 @@ function generate(target) {
     }
 }
 function clean() {
-    let bottom = camera.peak + camera.dip;
+    let bottom = camera.height + camera.dip;
     platforms = platforms.filter(p => p.y < bottom);
 }
 
